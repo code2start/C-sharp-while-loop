ec73c26c70ffac4b4d1c61dc119dbb4d9ae7c1a2
Mohamed Fathy <codetwostart@gmail.com>
2019-10-12 13:57:52 +0200

    Initial comment

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace whileloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 5;
+            while(counter < 5){
+                Console.WriteLine("{0}- Hello ",counter+1);
+                counter++;
+            }
+            
+           
+        }
+    }
+}
